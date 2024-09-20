978075c7d0675b66150cea9d6cdfe182f8930b04
Emma Gachancipa <julianaga01@gmail.com>
2024-09-20 07:49:28 -0500

    Include status validation

--- consulting_ms/controllers/re_property.py
+++ consulting_ms/controllers/re_property.py
@@ -1,5 +1,10 @@
+from fastapi import HTTPException
+
 from api.schemas.re_property import REPropertySchema
 from infrastructure.sql.repositories.re_property import REPropertyRepository
+from settings import Settings
+
+settings = Settings()
 
 
 class REPropertyController:
@@ -7,5 +12,14 @@ class REPropertyController:
         self.re_property_repository = REPropertyRepository()
 
     # def get_filtering(self, re_property: REPropertySchema):
-    def get_filtering(self):
-        return self.re_property_repository.get_filtering()
+    def get_filtering(self, year: int, city: str, status_list: list[str]):
+        # Validate the status parameter
+        if status_list:
+            for status in status_list:
+                if status not in settings.VALID_STATUSES:
+                    raise HTTPException(
+                        status_code=400,
+                        detail=f"Invalid status value. Valid options are: {settings.VALID_STATUSES}",
+                    )
+
+        return self.re_property_repository.get_filtering(year, city, status_list)
