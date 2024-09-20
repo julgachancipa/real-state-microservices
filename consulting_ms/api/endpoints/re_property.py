2a70b3f2e3ece5af921d113280a325d8522677bd
Emma Gachancipa <julianaga01@gmail.com>
2024-09-20 07:46:27 -0500

    Include properties and status endpoints
    properties -> allows to find properties using filters
    status -> allow us to check that the MS is working

--- consulting_ms/api/endpoints/re_property.py
+++ consulting_ms/api/endpoints/re_property.py
@@ -1,11 +1,33 @@
-from fastapi import APIRouter
+from typing import List, Union
 
-from api.schemas.re_property import REPropertySchema
+from fastapi import APIRouter, Depends
+
+from api.schemas.re_property import MultiStatusResponse, QueryParams, REPropertySchema
 from controllers.re_property import REPropertyController
 
 router = APIRouter()
 
 
-@router.get("/properties")
-def get_properties():
-    return REPropertyController().get_filtering()
+@router.get(
+    "/properties",
+    response_model=List[REPropertySchema],
+    responses={
+        207: {
+            "description": "Multi-Status Response, some properties have missing data",
+            "model": MultiStatusResponse,
+        },
+        400: {
+            "description": "Bad Request, invalid input information",
+            "content": {"application/json": {"example": {"detail": "Invalid input"}}},
+        },
+    },
+)
+def get_properties(query: QueryParams = Depends()):
+    return REPropertyController().get_filtering(
+        query.year, query.city, query.status_list
+    )
+
+
+@router.get("/status")
+def get_status():
+    return {"status": "ok"}
